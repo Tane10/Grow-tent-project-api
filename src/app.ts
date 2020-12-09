4a723afaaba53dc172931e71bb18ba517e12fc84
JoshThom-itv <joshua.thomas.houlker@itv.com>
2020-12-09 21:07:16 +0000

    updated everything

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express, { Application } from "express";
+import cors, { CorsOptions } from "cors";
+import helmet from "helmet";
+import routes from "./routes";
+
+const app: Application = express();
+
+require("dotenv").config()
+
+/** 
+ * options for cors midddleware
+ * Documentation: https://expressjs.com/en/resources/middleware/cors.html
+ */
+const options: CorsOptions = {
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE"
+};
+
+// middleware
+app.use(helmet());
+app.use(cors(options));
+app.use(express.json());
+app.set("port", process.env.PORT || 8080);
+app.options("*", cors(options)); //enable pre-flight
+
+// use all endpoints
+app.use(routes);
+
+try {
+    app.listen(app.get("port"), () => {
+        console.log("the server is running on port",
+            app.get("port")
+        );
+    });
+
+} catch (err) {
+    console.log(err);
+}
---
+++ src/routes.ts
@@ -0,0 +1,29 @@
+import express, { Router, Response, Request } from "express";
+// import { login } from "./controllers/login_controller";
+// import { registerUser, updateUser, deleteUser, getUsers } from "./controllers/user_controller";
+// import { uploadImage } from "./controllers/upload_controller";
+// import { searchImage } from "./controllers/search_controller"
+
+var router: Router = express.Router();
+
+// need to run in cloud env
+router.get("/", ((req: Request, res: Response) => { res.send('root'); }));
+
+router.post("/api/v1/login", (login)=>{
+
+});
+
+router.get("/api/v1/temps", (getTemps) =>{
+
+});
+
+router.post("/api/v1/image/upload", (upload) =>{
+
+});
+
+router.get("/api/v1/image/get", (getImages) => {
+
+});
+
+
+export default router;
