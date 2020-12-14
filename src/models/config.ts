bd8ede9a855750cf646476adab8ba0270f9486c3
JoshThom-itv <joshua.thomas.houlker@itv.com>
2020-12-14 09:38:05 +0000

    connected to DB

---
+++ src/models/config.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+require("dotenv").config();
+
+export default async function connectToPlantDB(): Promise<void> {
+    mongoose.Promise = global.Promise;
+
+    mongoose.connection.once("open", () => console.log("connected to database"));
+
+    try {
+        mongoose.connect(`${process.env.MONGO_CONNECTION_STRING}`, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "plant_api" })
+    } catch (error) {
+        console.log(error)
+    }
+}
