b8548be5783605a77878752508660d15742475e6
JoshThom-itv <joshua.thomas.houlker@itv.com>
2020-12-14 19:05:46 +0000

    we can now sign tokens

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,21 +1,93 @@
 import { Request, Response } from "express";
-import UserService from "../services/user_service";
+import { UserDetails } from "../types";
+import UserModel from "../models/users";
+import { CustomError } from "../error";
+import { Document } from "mongoose";
+import AuthService from "../services/auth_service";
+import jwt from "jsonwebtoken";
+require("dotenv").config();
 
 export async function signUp(req: Request, res: Response) {
-    const addNewUser = await UserService.signUpUser(req.body);
+    const userDetails: UserDetails = req.body;
 
-    if(addNewUser === undefined) res.status(200).send("User created")
-    else{
-        res.send(addNewUser)
-    }
+    await UserModel.find({ email: userDetails.email })
+        .then((user) => {
+            if (user.length >= 1) {
+                const duplicateUser: CustomError = {
+                    statusCode: 409,
+                    message: "Auth Error"
+                }
+                res.send(duplicateUser);
+            }
+            else {
+                AuthService.hashPassword(userDetails.password, 12, (err, hash) => {
+                    if (err) {
+                        const hashError: CustomError = {
+                            statusCode: 400,
+                            message: err.message
+                        }
+                        res.send(hashError);
+                    } else {
+                        const newUser: Document = new UserModel({
+                            email: userDetails.email,
+                            password: hash
+                        });
+
+                        newUser.save()
+                            .then(() => {
+                                res.send("User created")
+                            }).catch(err => {
+                                const newUserErr: CustomError = {
+                                    statusCode: 400,
+                                    message: err.message
+                                }
+                                res.send(newUserErr)
+                            })
+                    }
+                });
+            }
+        });
+
 }
 
 
 export async function login(req: Request, res: Response) {
-    const loginUser = await UserService.loginUser(req.body);
+    const userDetails: UserDetails = req.body;
+    await UserModel.find({ email: userDetails.email })
+        .then((user) => {
+            if (user.length === 0) {
+                const noUser: CustomError = {
+                    statusCode: 400,
+                    message: "Auth Error"
+                }
 
-    if(loginUser === undefined) res.status(200).send("Login")
-    else{
-        res.send(loginUser)
-    }
+                res.send(noUser)
+            } else {
+                user[0].get("password");
+                AuthService.compare(userDetails.password, user[0].get("password"), (error: string | null, match: boolean | null) => {
+                    if (error) {
+                        const password: CustomError = {
+                            statusCode: 401,
+                            message: "Auth Error"
+                        }
+                        res.send(password)
+                    }
+                    if (match) {
+
+                        const token = jwt.sign({
+                            email: user[0].get("email"),
+                            userId: user[0].get("_id")
+                        }, `${process.env.JWT_KEY}`, {
+                            expiresIn: "1h"
+                        })
+
+                        res.status(200).send({
+                            message: "Auth successful",
+                            token: token
+                        })
+                    }
+                })
+
+            }
+        });
 }
--- src/services/auth_service.ts
+++ src/services/auth_service.ts
@@ -1,15 +1,15 @@
 import bcrypt from "bcrypt";
 
 export default class AuthService {
-    public static hashPassword(password: string, rounds: number, callback: (error: Error, hash: string) => void) : void {
+    public static hashPassword(password: string, rounds: number, callback: (error: Error, hash: string) => void): void {
         bcrypt.hash(password, rounds, (error, hash) => {
             callback(error, hash);
         });
     }
 
-    public static compare(password: string, dbHash: string, callback: (error: string | null, match: boolean | null) => void) {
+    public static compare(password: string, dbHash: string, callback: (error: string | null, match: boolean | null) => void): void {
         bcrypt.compare(password, dbHash, (err: Error, match: boolean) => {
-            if(match) {
+            if (match) {
                 // passwords match
                 callback(null, true);
             } else {
