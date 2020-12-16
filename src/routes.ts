5c3bbc1d000c8a50788193906becd0fb02b81366
JoshThom-itv <joshua.thomas.houlker@itv.com>
2020-12-16 13:17:25 +0000

    added jwt to image upload

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import AuthService from "./services/auth_service";
 import UploadService from "./services/upload_service";
 import cloudinary from "cloudinary";
 import multer from "multer";
-import  { CloudinaryStorage } from "multer-storage-cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_NAME,
@@ -31,13 +31,10 @@ router.post("/api/v1/login", UserController.login);
 
 router.post("/api/v1/signup", AuthService.checkIfVaild, UserController.signUp);
 
-router.get("/api/v1/temps", (getTemps) => { });
+router.get("/api/v1/temps", (getTemps) => {});
 
-router.post("/api/v1/image/upload", parser.single("image"), UploadService.imageUpload);
+router.post("/api/v1/image/upload", AuthService.checkIfVaild, parser.single("image"), UploadService.imageUpload);
 
-router.get("/api/v1/image/get", (getImages) => {
-
-});
-
+router.get("/api/v1/image/get", (getImages) => {});
 
 export default router;
